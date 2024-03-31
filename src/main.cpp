b4f65e705de2fb887d7779a0b58ca6e7088d19ee
Anna Goncharova <147106427+Goanni@users.noreply.github.com>
2024-03-31 03:17:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "Automata.h"
 
+int main() {
+    Automata Machine;
+
+    Machine.on();
+    Machine.getMenu();
+    Machine.coin(100);
+
+    Machine.off();
+  return 0;
+}
